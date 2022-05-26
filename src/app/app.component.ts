83d08e9e5aec2ebe21379e381194cc4b27e99d62
Александр Субботин <a.p.subbotin@tinkoff.ru>
2022-05-27 01:19:15 +0300

    add icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {Character} from "./services/models/character";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,17 @@ import {Character} from "./services/models/character";
 export class AppComponent {
   title = 'fcs-web';
 
-  curCharacter: Character | undefined;
-
+  constructor(iconRegistry : MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('delete', sanitizer.bypassSecurityTrustResourceUrl("../icons/delete.svg"));
+    iconRegistry.addSvgIcon('heart', sanitizer.bypassSecurityTrustResourceUrl("../icons/cards-heart.svg"));
+    iconRegistry.addSvgIcon('bag',sanitizer.bypassSecurityTrustResourceUrl("../icons/bag.svg"));
+    iconRegistry.addSvgIcon('edit',sanitizer.bypassSecurityTrustResourceUrl("../icons/border-color.svg"));
+    iconRegistry.addSvgIcon('star-outline',sanitizer.bypassSecurityTrustResourceUrl("../icons/star-outline.svg"))
+    iconRegistry.addSvgIcon('star',sanitizer.bypassSecurityTrustResourceUrl("../icons/star.svg"));
+    iconRegistry.addSvgIcon('piggy',sanitizer.bypassSecurityTrustResourceUrl("../icons/piggy-bank.svg"));
+    iconRegistry.addSvgIcon('close',sanitizer.bypassSecurityTrustResourceUrl("../icons/close.svg"));
+    iconRegistry.addSvgIcon('accept',sanitizer.bypassSecurityTrustResourceUrl("../icons/check.svg"));
+    iconRegistry.addSvgIcon('dice',sanitizer.bypassSecurityTrustResourceUrl("../icons/logo.svg"));
+  }
 
 }
