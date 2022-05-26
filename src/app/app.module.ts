b53908595f1b668b2d8cbdf34e2f1d0db60ffa05
Александр Субботин <a.p.subbotin@tinkoff.ru>
2022-05-27 01:22:12 +0300

    deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatTabsModule} from "@angular/material/tabs";
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
-import {RouterModule} from "@angular/router";
 import { MainPageComponent } from './main-page/main-page.component';
 import {AppRoutingModule} from "./app-routing/app-routing.module";
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -23,6 +22,27 @@ import { CreateCharacterComponent } from './create-character/create-character.co
 import { ChooseDialogComponent } from './create-character/choose.dialog/choose-dialog.component';
 import {MatSelectModule} from "@angular/material/select";
 import {MatDialogModule} from "@angular/material/dialog";
+import { MatChipsModule} from "@angular/material/chips";
+import { ChracterSheetComponent } from './character-infofull/chracter-sheet/chracter-sheet.component';
+import {MatSliderModule} from "@angular/material/slider";
+import { CharacterPanelComponent } from './additional-components/character-panel/character-panel.component';
+import { SkillPanelComponent } from './additional-components/skill-panel/skill-panel.component';
+import { CharacterAttributesComponentComponent } from './character-infofull/chracter-sheet/character-attributes-component/character-attributes-component.component';
+import {MatExpansionModule} from "@angular/material/expansion";
+import { CharacterAttributePanelComponent } from './character-infofull/chracter-sheet/character-attributes-component/character-attriute-panel/character-attribute-panel.component';
+import { SliderWithValuesComponent } from './character-infofull/chracter-sheet/slider-with-values/slider-with-values.component';
+import { CustomCreateCharacterComponent } from './create-character/custom-create-character/custom-create-character.component';
+import { DndCreateCharacterComponent } from './create-character/dnd-create-character/dnd-create-character.component';
+import { CharacterListCardComponent } from './character-list/character-list-card/character-list-card.component';
+import { SkillsPageComponent } from './character-infofull/skills-page/skills-page.component';
+import { PrimaryChipsComponent } from './additional-components/character-panel/primary-chips/primary-chips.component';
+import { SpellPanelComponent } from './additional-components/spell-panel/spell-panel.component';
+import { SpellsPageComponent } from './character-infofull/spells-page/spells-page.component';
+import { InventoryPageComponent } from './character-infofull/inventory-page/inventory-page.component';
+import { ItemPanelComponent } from './additional-components/item-panel/item-panel.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
+import { RollingDialogComponent } from './additional-components/rolling-dialog/rolling-dialog.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +52,23 @@ import {MatDialogModule} from "@angular/material/dialog";
     AuthPageComponent,
     MainPageComponent,
     CreateCharacterComponent,
-    ChooseDialogComponent
+    ChooseDialogComponent,
+    ChracterSheetComponent,
+    CharacterPanelComponent,
+    SkillPanelComponent,
+    CharacterAttributesComponentComponent,
+    CharacterAttributePanelComponent,
+    SliderWithValuesComponent,
+    CustomCreateCharacterComponent,
+    DndCreateCharacterComponent,
+    CharacterListCardComponent,
+    SkillsPageComponent,
+    PrimaryChipsComponent,
+    SpellPanelComponent,
+    SpellsPageComponent,
+    InventoryPageComponent,
+    ItemPanelComponent,
+    RollingDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -48,14 +84,21 @@ import {MatDialogModule} from "@angular/material/dialog";
     MatButtonModule,
     MatButtonToggleModule,
     MatSelectModule,
-    MatDialogModule
+    MatDialogModule,
+    MatChipsModule,
+    MatSliderModule,
+    MatExpansionModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatSnackBarModule
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
-    }
+    },
+    MatSnackBarModule
   ],
   bootstrap: [AppComponent]
 })
