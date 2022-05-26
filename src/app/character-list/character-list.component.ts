a00271f056970834bad039060511fbbd28381d0b
Александр Субботин <a.p.subbotin@tinkoff.ru>
2022-05-27 01:23:13 +0300

    character list

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,20 +1,24 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import { Character } from '../services/models/character';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Character} from '../services/models/character';
 import {CharacterInfoProviderService} from "../services/character-info-provider.service";
 import {CharacterService} from "../services/character.service";
 import {MatDialog} from "@angular/material/dialog";
 import {ChooseDialogComponent} from "../create-character/choose.dialog/choose-dialog.component";
 import {Router} from "@angular/router";
+import {AsyncSubject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.css'],
+  styleUrls: ['./character-list.component.scss'],
   providers: [CharacterInfoProviderService]
 
 })
-export class CharacterListComponent implements OnInit {
+export class CharacterListComponent implements OnInit, OnDestroy {
 
+  private destroy$ : AsyncSubject<any> = new AsyncSubject<any>();
+
+  curId: number = -1;
   characters: Character[] = [];
 
   //@Input() curCharacter: Character | undefined;
@@ -39,18 +43,45 @@ export class CharacterListComponent implements OnInit {
 
   ngOnInit(): void {
     this.characterService.getCharactersList(1).subscribe((data:any) => this.characters=data);
+    this.curCharacterService
+      .character
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe(
+      (character) =>{
+        if(character) {
+          this.curId = character.id;
+        }
+      }
+    );
+
+    this.curCharacterService
+      .InfoUpdates$
+      .subscribe(
+        (val) => {
+          this.characters.map((character) => {
+            if(character.id == val.id) {
+              character.description = val.description;
+            }
+          })
+        }
+      )
     console.log(this.characters.length)
   }
 
   onAddCharacterButtonClick() {
     const dialogRef = this.dialog.open(ChooseDialogComponent, {
-      width: '250px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      const template = result;
-      this.router.navigate(['create',template])
+      console.log('The dialog was closed' + result);
+      this.router.navigate(['create',result]);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete()
   }
 }
