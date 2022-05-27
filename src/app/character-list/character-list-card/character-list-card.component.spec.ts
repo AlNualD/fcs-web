43d1a6c18637ba4b040e03d75424a919d70dafd4
Александр Субботин <a.p.subbotin@tinkoff.ru>
2022-05-27 23:05:59 +0300

    additional

---
+++ src/app/character-list/character-list-card/character-list-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CharacterListCardComponent } from './character-list-card.component';
+
+describe('CharacterListCardComponent', () => {
+  let component: CharacterListCardComponent;
+  let fixture: ComponentFixture<CharacterListCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CharacterListCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CharacterListCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/character-list/character-list-card/character-list-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-list-card',
+  templateUrl: './character-list-card.component.html',
+  styleUrls: ['./character-list-card.component.css']
+})
+export class CharacterListCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
